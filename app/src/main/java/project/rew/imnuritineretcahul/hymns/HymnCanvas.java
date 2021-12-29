0caff622ab30ef9e125a100bb50417ee6b0a5576
VITP <vadim-plamadeala@mail.ru>
2021-12-29 12:35:33 +0200

    SeekBar for text size (buttom bags and sizing in html)

--- app/src/main/java/project/rew/imnuritineretcahul/hymns/HymnCanvas.java
+++ app/src/main/java/project/rew/imnuritineretcahul/hymns/HymnCanvas.java
@@ -6,13 +6,18 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.view.Window;
 import android.webkit.WebView;
+import android.widget.SeekBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import project.rew.imnuritineretcahul.R;
+import project.rew.imnuritineretcahul.utils.PrefConfig;
 import project.rew.imnuritineretcahul.utils.Utils;
 
 public class HymnCanvas extends AppCompatActivity {
+
+    int seekvalue;
+    int minsbprogress = 30;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,7 @@ public class HymnCanvas extends AppCompatActivity {
         setContentView(R.layout.activity_hymn_canvas);
         int id = getIntent().getIntExtra("id", 0);
         int nr = getIntent().getIntExtra("nr", 0);
+        seekvalue = PrefConfig.load_saved_progress(this);
         Hymn hymn = Utils.hymns.get(nr - 1);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(hymn.getTitle());
@@ -27,7 +33,36 @@ public class HymnCanvas extends AppCompatActivity {
         WebView webView = findViewById(R.id.wvCanvas);
         webView.getSettings().setJavaScriptEnabled(true);
         String content = Utils.readContent(id, true, getApplicationContext());
-        webView.loadDataWithBaseURL(null, content, "text/html", "utf-8", null);
+        webView.loadDataWithBaseURL(null, "<font size=" +'"'+ String.valueOf(seekvalue/10)+"px" +'"'+">"+ content + "</font>", "text/html", "utf-8", null);
+        SeekBar seekBar = (SeekBar) findViewById(R.id.seekbar);
+        seekBar.setProgress(seekvalue);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                seekvalue = i;
+                if (seekvalue < minsbprogress) {
+                    seekvalue = minsbprogress;
+                }
+                webView.loadDataWithBaseURL(null, "<font size=" +'"'+ String.valueOf(seekvalue/10)+"px" +'"'+">"+ content + "</font>", "text/html", "utf-8", null);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                if (seekvalue < minsbprogress) {
+                    seekvalue = minsbprogress;
+                }
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                if (seekvalue < minsbprogress) {
+                    seekvalue = minsbprogress;
+                }
+
+                PrefConfig.SaveSBprogress(getApplicationContext(), seekvalue);
+            }
+        });
     }
 
     @Override
